36ab5f4963f695547b2689d5b7ece5a6aa41adfa
Matt Foster <fmatthew40@gmail.com>
2022-03-27 19:14:05 -0700

    add friends and reactions

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,8 @@ import NoMatch from './pages/NoMatch';
 import SingleThought from './pages/SingleThought'; 
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
+import { setContext } from '@apollo/client/link/context';
+
 
 
 import Home from './pages/Home';
@@ -17,10 +19,20 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 })
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
-})
+});
 
 function App() {
   return (
